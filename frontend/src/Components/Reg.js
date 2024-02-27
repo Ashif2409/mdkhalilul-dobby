b5c5a752ce812b6b98cc2ec460b93d1aa895ee6b
Md Khalilul Rahman <105421454+Ashif2409@users.noreply.github.com>
2024-02-27 22:51:56 +0530

    Update Reg.js

--- frontend/src/Components/Reg.js
+++ frontend/src/Components/Reg.js
@@ -22,7 +22,7 @@ export const Reg = () => {
         const { email, name, password, confirmPass } = user;
         if (email && name && password && confirmPass) {
             if (password === confirmPass) {
-                axios.post('https://mdkhalilul-dobby-api.vercel.appregister', user)
+                axios.post('https://mdkhalilul-dobby-api.vercel.appregister/register', user)
                     .then(res => {
                         navigate('/');
                         console.log('Successfully Register');
